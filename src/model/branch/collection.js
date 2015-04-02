7f4f65ad6b7641519f5427d1932cc869df3f072f
sguerra <sergioguerra1991@gmail.com>
2015-04-01 23:28:30 -0600

    DB::  firebase database changed

--- src/model/branch/collection.js
+++ src/model/branch/collection.js
@@ -3,6 +3,6 @@ import Branch from './model';
 export default Backbone.Firebase.Collection.extend({
 
 	model : Branch,
-	url: "https://resplendent-inferno-3916.firebaseio.com/branches"
+	url: "https://sc-tickets-rosedal.firebaseio.com/branches"
 
 });
--- src/model/collection.js
+++ src/model/collection.js
@@ -3,6 +3,6 @@ import Ticket from './model';
 export default Backbone.Firebase.Collection.extend({
 
 	model : Ticket,
-	url: "https://resplendent-inferno-3916.firebaseio.com/tickets"
+	url: "https://sc-tickets-rosedal.firebaseio.com/tickets"
 
 });
